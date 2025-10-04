2460cecb5005d831f067713c3be2da8bbb214b9e
sohaib757 <1000065@spsd.sk.ca>
2025-10-03 22:33:51 -0600

    improved promotion added comments

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -6,31 +6,55 @@
 // - Created a functioning timer using the setInterval() function
 // - Added text that changes size based on the screen size.
 
+// Global Variables 
+
+// Chess piece images
 let whitePawnImg;
 let blackPawnImg;
+let whiteQueenImg;
+let blackQueenImg;
+
+// Size of each square on the board depending on screen size
 let size;
+
+// State variables
 let whitePawnOneClicked = false;
 let blackPawnOneClicked = false;
-let whitePawnOneForward = 0;
-let blackPawnOneForward = 0;
 let whitePawnOneTurn = true;
 let firstMove = true;
 let blackPawnOneTurn = false;
 let canMoveW = false;
 let canMoveB = false;
-let whiteCircleY = 4.5;
+let whitePromoting = false;
+let blackPromoting = false;
+let whitePromoted = false;
+let blackPromoted = false;
+
+// Adjust pawn postion
+let whitePawnOneForward = 0;
+let blackPawnOneForward = 0;
 let changePawnW = 0;
 let changePawnB = 0;
+
+// Adjust circle position
 let changeCircleW = 0;
 let changeCircleB = 0;
 let blackCircleY= 2.5;
+let whiteCircleY = 4.5;
+
+// Time for both colours in seconds
 let timeW = 30;
 let timeB = 30;
 
+// Preloads the images of the chess pieces before initializing the game
 function preload() {
   whitePawnImg = loadImage("whitepawn.png");
   blackPawnImg = loadImage("blackpawn.png");
+  whiteQueenImg = loadImage("whitequeen.jpg");
+  blackQueenImg = loadImage("blackqueen.png")
 }
+
+// Sets window size depending on the size of the user's screen and controls timer
 function setup() {
   if (windowWidth > windowHeight) {
     createCanvas(windowHeight, windowHeight);
@@ -39,9 +63,12 @@ function setup() {
     createCanvas(windowWidth, windowWidth);
   }
   size = width/8;
+  
+  // Start timer that runs every one second
   setInterval(timer, 1000);
 }
 
+// Calls the functions needed for the experience to run
 function draw() {
   background(220);
   showBoard();
@@ -52,9 +79,11 @@ function draw() {
   keyPressed();
   showTimer();
   gameOver();
+  // Scale text size according to canvas size
   textSize(width/40);
 }
 
+// Resizes the window in accordance to the user's screen size
 function windowResized() {
   if (windowWidth > windowHeight) {
     resizeCanvas(windowHeight, windowHeight);
@@ -65,6 +94,7 @@ function windowResized() {
   size = width/8;
 }
 
+// Displays an 8x8 chess board that also adjusts to the screen size
 function showBoard() {
   let isWhite = false;
   for (let y = 0; y < 8; y ++){
@@ -82,35 +112,49 @@ function showBoard() {
   }
 }
 
+// Displays the pawn images in their respective positions
 function showPawns() {
   image(whitePawnImg, size/2 - size, 6 * size - whitePawnOneForward, size * 2, size);
   image(blackPawnImg, size/2, size + blackPawnOneForward, size *2, size);
 }
 
+// Controls whether or not pawns can be moved and the positions of the circles
 function movePawns() {
+  
+  // Check if white pawn is in first move to determine how many squares it can move
   if (whitePawnOneClicked && firstMove) {
     fill("grey");
     circle(size/2, size * whiteCircleY, size/4);
     circle(size/2, size * (whiteCircleY + 1), size/4);
     canMoveW = true;
   }
+  
+  // Make sure white pawn can only move when clicked
   if (!whitePawnOneClicked) {
     canMoveW = false;
   }
+  
+  // White pawn can only move 1 square after first move
   else if (whitePawnOneClicked && !firstMove) {
     fill("grey");
     circle(size/2, 6 * size - changePawnW - changeCircleW - size/2, size/4);
     canMoveW = true;
   }
+  
+  // Check if black pawn is in first move to determine how many squares it can move
   if (blackPawnOneClicked && firstMove) {
     fill("grey");
     circle(size * 1.5, size * blackCircleY, size/4);
     circle(size * 1.5, size * (blackCircleY + 1), size/4);
     canMoveB = true;
   }
+  
+  // Make sure black pawn can only move when clicked
   if (!blackPawnOneClicked) {
     canMoveB = false;
   }
+  
+  // Black pawn can only move 1 square after first move
   else if (blackPawnOneClicked && !firstMove) {
     fill("grey");
     circle(size * 1.5, 2 * size + changePawnB + changeCircleB + size/2, size/4);
@@ -118,13 +162,18 @@ function movePawns() {
   }
 }
 
+// Controls the movement of the pawns based on mouse position and states
 function mouseClicked() {
+  
+  // Allows white pawn to move only when it is white turn and when the mouse is on the pawn
   if (whitePawnOneTurn && mouseX < size && mouseY > 6 * size - changePawnW && mouseY < 7 * size - changePawnW) {
     whitePawnOneClicked = true;
   }
   else {
     whitePawnOneClicked = false; 
   }
+  
+  // White pawn move 1 square forward
   if (canMoveW && whitePawnOneTurn && mouseX < size && mouseY < size * 6 - changePawnW && mouseY > size * 5 - changePawnW) {
     whitePawnOneForward += size;
     whitePawnOneTurn = false;
@@ -134,15 +183,19 @@ function mouseClicked() {
     changePawnW += size;
     changeCircleW += 1;
   }
+  
+  // White pawn move 2 squares forward only when it's the first move
   else if (canMoveW && firstMove && whitePawnOneTurn && mouseX <  size && mouseY < size * 5 - changePawnW && mouseY > size * 4 - changePawnW) {
     whitePawnOneForward += size * 2;
     whitePawnOneTurn = false;
     blackPawnOneTurn = true;
     canMoveB = true;
-    canMoveW= false;
+    canMoveW = false;
     changePawnW += 2 * size;
     changeCircleW += 1;
   }
+  
+  // Allows black pawn to move only when it is black turn and when the mouse is on the pawn
   if (blackPawnOneTurn && mouseX < 2 * size  && mouseX > size && mouseY > size + changePawnB && mouseY < 2 * size + changePawnB) {
     blackPawnOneClicked = true;
     firstMoveB = true;
@@ -150,6 +203,8 @@ function mouseClicked() {
   else {
     blackPawnOneClicked = false;
   }
+  
+  // Black pawn move 1 square forward
   if (canMoveB && blackPawnOneTurn && mouseX < 2 * size  && mouseX > size && mouseY < size * 3 + changePawnB && mouseY > size * 2 + changePawnB) {
     blackPawnOneForward += size;
     blackPawnOneTurn = false;
@@ -160,6 +215,8 @@ function mouseClicked() {
     changePawnB += size;
     changeCircleB += 1;
   }
+ 
+  // Black pawn move 2 squares forward only when it's the first move
   else if (canMoveB && firstMove && blackPawnOneTurn && mouseX < size * 2 && mouseX > size && mouseY < size * 4 + changePawnB && mouseY > size * 3 + changePawnB) {
     blackPawnOneForward += size * 2;
     blackPawnOneTurn = false;
@@ -172,25 +229,71 @@ function mouseClicked() {
   }
 }
 
+// Promotes from pawn to queen when applicable
 function promotion() {
-  if (changePawnW === 6 * size) {
-    canMoveB = false;
+  // White Pawn Promotion
+  if (changePawnW === 6 * size && !whitePromoted && !whitePromoting) {
+    whitePromoting = true;
+    canMoveW = false;
+    canMoveB = false; 
+  }
+
+  if (whitePromoting) {
+    // Display message for white promotion
     fill("grey");
-    rect(2 * size, 0, size * 3, size);
-    if (mouseX <= 3 * size && mouseX >= 2 * size && mouseY <= size) {
-      circle(size/2,size/2,50);
+    rect(width/2 - size, height/2 - size, 2*size, size);
+    fill("red");
+    text("Press 'p' for queen.", width/2 - size/1.2, height/2 - size/1.7, size*3, size);
+    blackPawnOneTurn = false;
+    
+    // Promote when p is pressed
+    if (keyIsDown(80)) { 
+      whitePromoting = false;
+      whitePromoted = true;
+      canMoveB = true;    
+      whitePawnOneTurn = false;
+      blackPawnOneTurn = true;
     }
   }
-  else if (changePawnB === 6 * size) {
+
+  // Black Pawn Promotion 
+  if (changePawnB === 6 * size && !blackPromoted && !blackPromoting) {
+    blackPromoting = true;   
+    canMoveB = false;     
+    canMoveW = false;  
+  }
+
+  if (blackPromoting) {
+    // Display message for black promotion
     fill("grey");
-    rect(2 * size, 0, size * 3, size);
-    if (mouseX <= 3 * size && mouseX >= 2 * size && mouseY <= size) {
-      circle(size/2,size/2,50);
+    rect(width/2 - size, height/2 - size, 2*size, size);
+    fill("red");
+    text("Press 'u' for queen.", width/2 - size/1.2, height/2 - size/1.7, size*3, size);
+    whitePawnOneTurn = false;
+    
+    // Promote when u is pressed
+    if (keyIsDown(85)) { 
+      blackPromoting = false;
+      blackPromoted = true; 
+      canMoveW = true;  
+      whitePawnOneTurn = true;
+      blackPawnOneTurn = false;
     }
-  }  
+  }
+
+  // Promote from pawn to queen by changing image
+  if (whitePromoted) {
+    image(whiteQueenImg, 0, 0, size, size);
+  }
+  if (blackPromoted) {
+    image(blackQueenImg, size, size * 7, size, size);
+  }
 }
 
+// Can be used to change the colour of the chess board
 function keyPressed() {
+  
+  // Tan/brown board if user presses q
   if (key === 'q') {
     let isWhite = false;
     for (let y = 0; y < 8; y ++){
@@ -210,6 +313,8 @@ function keyPressed() {
     movePawns();
     promotion();
   }
+  
+  // Return to white/black theme if user presses e
   if (key === 'e') {
     let isWhite = false;
     for (let y = 0; y < 8; y ++){
@@ -231,43 +336,64 @@ function keyPressed() {
   promotion();
 }
 
+// Displays the timer for both white and black
 function showTimer() {
+  
+  // White timer
   fill("green");
   rect(width-width/15, height/900, width/8, height/30);
   fill("red");
   text(timeW, width-width/25, height/40);
+  
+  // Black timer
   fill("green");
   rect(width - width/15, height - size/4, width/8, height/30);
   fill("red");
   text(timeB, width-width/25, height - height/150);
 }
 
+// A timer that decreases for each colour when its their turn and when their time is positive
 function timer() {
-  if((whitePawnOneTurn || changePawnW === 6 * size) && changePawnB !== 6 * size && timeW >= 0){
+  if((whitePawnOneTurn || whitePromoting) && timeW >= 0){
     timeW --;
   }
-  if((blackPawnOneTurn|| changePawnB === 6 * size) && changePawnW !== 6 * size && timeB >= 0) {
+  if((blackPawnOneTurn || blackPromoting) && timeB >= 0) {
     timeB --;
   }
 }
 
+// Ends the game if either side runs out of time
 function gameOver() {
+  
+  // Gives black the win if white runs out of time 
   if (timeW < 0) {
     fill("blue");
     rect(width/2 - size, height/2 - size, 2*size, size);
+    rect(width/2 - size, height/1.7 - size, 2*size, size);
     fill(255,255,0);
-    text("Black wins on time", width/2- size/1.2, height/2 - size/1.7, size*3, size);
+    
+    // Displays text declaring black as the winner and how to restart
+    text("Black wins on time.", width/2- size/1.2, height/2 - size/1.7, size*3, size);
+    text("Press R to restart.", width/2- size/1.2, height/1.7 - size/1.7, size*3, size);
     canMoveW = false;
     canMoveB = false;
   }
+  
+  // Gives white the win if black runs out of time
   if (timeB < 0) {
     fill("blue");
     rect(width/2 - size, height/2 - size, 2*size, size);
+    rect(width/2 - size, height/1.7 - size, 2*size, size);
     fill(255,255,0);
+    
+    // Displays text declaring white as the winner and how to restart
     text("White wins on time", width/2- size/1.2, height/2 - size/1.7, size*3, size);
+    text("Press R to restart.", width/2- size/1.2, height/1.7 - size/1.7, size*3, size);
     canMoveW = false;
     canMoveB = false;
   }
+  
+  // Restarts the experience after time runs out for either side and r is clicked 
   if (keyIsDown(82) && (timeB < 0 || timeW < 0)) {
     whitePawnOneClicked = false;
     blackPawnOneClicked = false;
@@ -286,5 +412,9 @@ function gameOver() {
     blackCircleY= 2.5;
     timeW = 30;
     timeB = 30;
+    whitePromoting = false;
+    blackPromoting = false;
+    whitePromoted = false;
+    blackPromoted = false;
   }
 }
