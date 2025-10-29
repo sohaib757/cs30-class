d9ace0c201590f261a00bca3e1e7c5d3e574af3b
sohaib757 <1000065@spsd.sk.ca>
2025-10-29 09:11:50 -0600

    2d array

--- 2darrays/sketch.js
+++ 2darrays/sketch.js
@@ -6,9 +6,20 @@
 // - describe what you did to take this project "above and beyond"
 
 let theGrid = [];
+let cellSize;
+let cols;
+let rows;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
+  }
+  else if (windowHeight > windowWidth) {
+    createCanvas(windowWidth, windowWidth);
+  }
+  cellSize = width/8
+  cols = Math.floor(width/cellSize);
+  rows = Math.floor(height/cellSize);
 }
 
 function draw() {
@@ -17,5 +28,8 @@ function draw() {
 }
 
 function displayGrid() {
-  for (let y = 0; y < rows; y++)
+  for (let y = 0; y < rows; y++) {
+    theGrid.push([]);
+    for (let x = 0; x < cols; x++)
+  }
 }
