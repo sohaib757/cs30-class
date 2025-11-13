5334dd7504fe83b267d049467384d2b6e84fe9ff
sohaib757 <1000065@spsd.sk.ca>
2025-11-12 18:32:50 -0600

    capturing (white) complete

--- 2darrays/sketch.js
+++ 2darrays/sketch.js
@@ -164,7 +164,7 @@ function movePawns(x,y) {
   for (let whitePawn of whitePawnArray) {
     for (let blackPawn of blackPawnArray) {
       // Checks if white pawn can move 2 squares forward only on first move
-      if(whitePawn.firstMove && whitePawn.canMove && x === whitePawn.cols && y === whitePawn.rows - 2 && emptySquare(whitePawn.cols, whitePawn.rows - 1) === true && emptySquare(whitePawn.cols, whitePawn.rows - 2) === true) {
+      if(whitePawn.firstMove && whitePawn.canMove && x === whitePawn.cols && y === whitePawn.rows - 2 && checkSquare(whitePawn.cols, whitePawn.rows - 1) === "empty" && checkSquare(whitePawn.cols, whitePawn.rows - 2) === "empty") {
         whitePawn.rows -= 2;
         whitePawn.rows = constrain(whitePawn.rows, 0, 7);
         whiteTurn = false;
@@ -172,7 +172,7 @@ function movePawns(x,y) {
         whitePawn.firstMove = false;
       }
       // Checks if white pawn can move one square forward
-      else if (whitePawn.canMove && x === whitePawn.cols && y === whitePawn.rows - 1 && emptySquare(whitePawn.cols, whitePawn.rows - 1) === true) {
+      else if (whitePawn.canMove && x === whitePawn.cols && y === whitePawn.rows - 1 && checkSquare(whitePawn.cols, whitePawn.rows - 1) === "empty") {
         whitePawn.rows -= 1;
         whitePawn.rows = constrain(whitePawn.rows, 0, 7);
         whiteTurn = false;
@@ -180,17 +180,22 @@ function movePawns(x,y) {
         whitePawn.firstMove = false;
       }
       // Checks if white pawn can capture a black pawn
-      else if (whitePawn.canMove && (x === whitePawn.cols + 1 || x === whitePawn.cols - 1) && y === whitePawn.rows - 1 && emptySquare(x, whitePawn.rows - 1) === "black"){
+      else if (whitePawn.canMove && (x === whitePawn.cols + 1 || x === whitePawn.cols - 1) && y === whitePawn.rows - 1 && checkSquare(x, whitePawn.rows - 1) === "black"){
         whitePawn.cols = x;
+        whitePawn.cols = constrain(whitePawn.cols, 0, 7);
         whitePawn.rows -= 1;
         whitePawn.rows = constrain(whitePawn.rows, 0, 7);
         whiteTurn = false;
         blackTurn = true;
         whitePawn.firstMove = false;
-        blackPawnArray.splice(x,1);
+        for (let i = 0; i < blackPawnArray.length; i++) {
+          if (blackPawnArray[i].cols === x && blackPawnArray[i].rows === y) {
+          blackPawnArray.splice(i,1);
+          }
+        }
       }
       // Checks if black pawn can move 2 squares forward only on first move
-      if(blackPawn.firstMove && blackPawn.canMove && x === blackPawn.cols && y === blackPawn.rows + 2 && emptySquare(blackPawn.cols, blackPawn.rows + 1) === true && emptySquare(blackPawn.cols, blackPawn.rows + 2) === true) {
+      if (blackPawn.firstMove && blackPawn.canMove && x === blackPawn.cols && y === blackPawn.rows + 2 && checkSquare(blackPawn.cols, blackPawn.rows + 1) === "empty" && checkSquare(blackPawn.cols, blackPawn.rows + 2) === "empty") {
         blackPawn.rows += 2;
         blackPawn.rows = constrain(blackPawn.rows, 0, 7);
         blackTurn = false;
@@ -198,7 +203,7 @@ function movePawns(x,y) {
         blackPawn.firstMove = false;
       }
       // Checks if black pawn can move one square forward
-      else if (blackPawn.canMove && x === blackPawn.cols && y === blackPawn.rows + 1 && emptySquare(blackPawn.cols, blackPawn.rows + 1) === true) {
+      else if (blackPawn.canMove && x === blackPawn.cols && y === blackPawn.rows + 1 && checkSquare(blackPawn.cols, blackPawn.rows + 1) === "empty") {
         blackPawn.rows += 1;
         blackPawn.rows = constrain(blackPawn.rows, 0, 7);
         blackTurn = false;
@@ -209,61 +214,86 @@ function movePawns(x,y) {
   }
 }
 
-// Checks whether or not squares are empty
-function emptySquare(x,y) {
-  for (let whitePawn of whitePawnArray) {
-    if (whitePawn.cols === x && whitePawn.rows === y) {
-      return "white";
+// Checks whether or not squares are empty or occupied by a pawn and if so which colour
+function checkSquare(x,y) {
+  for (let row = 0; row < rows; row ++) {
+    for (let col = 0; col < cols ; col ++) {
+      theGrid[row][col] = "empty";
     }
+  }
+  for (let whitePawn of whitePawnArray) {
+    theGrid[whitePawn.rows][whitePawn.cols] = "white";
   }
   for (let blackPawn of blackPawnArray) {
-    if (blackPawn.cols === x && blackPawn.rows === y) {
-      return "black";
-    }
+    theGrid[blackPawn.rows][blackPawn.cols] = "black";
   }
-  return true;
+  return theGrid[y][x];
 }
 
 // Displays the potential moves for each pawn as grey circles
 function displayPossibleMoves() {
+  noStroke();
+  fill("grey");
   for (let whitePawn of whitePawnArray) {
     for (let blackPawn of blackPawnArray) {
+      // Displays three circles when white pawn can move forward twice and capture left (first move)
+      if (whiteTurn && whitePawn.pawnSelected && whitePawn.firstMove && checkSquare(whitePawn.cols - 1, whitePawn.rows - 1) === "black" && checkSquare(whitePawn.cols, whitePawn.rows - 1) === "empty" && checkSquare(whitePawn.cols, whitePawn.rows - 2)) {
+        circle(whitePawn.cols * cellSize + cellSize/2, whitePawn.rows * cellSize - cellSize/2, cellSize/4);
+        circle(whitePawn.cols * cellSize + cellSize/2, (whitePawn.rows - 1) * cellSize - cellSize/2, cellSize/4);
+        circle((whitePawn.cols - 1) * cellSize + cellSize/2, whitePawn.rows * cellSize - cellSize/2, cellSize/4);
+        whitePawn.canMove = true;
+      }
+      // Displays three circles when white pawn can move forward twice and capture right (first move)
+      else if (whiteTurn && whitePawn.pawnSelected && whitePawn.firstMove && checkSquare(whitePawn.cols + 1, whitePawn.rows - 1) === "black" && checkSquare(whitePawn.cols, whitePawn.rows - 1) === "empty" && checkSquare(whitePawn.cols, whitePawn.rows - 2)) {
+        circle(whitePawn.cols * cellSize + cellSize/2, whitePawn.rows * cellSize - cellSize/2, cellSize/4);
+        circle(whitePawn.cols * cellSize + cellSize/2, (whitePawn.rows - 1) * cellSize - cellSize/2, cellSize/4);
+        circle((whitePawn.cols + 1) * cellSize + cellSize/2, whitePawn.rows * cellSize - cellSize/2, cellSize/4);
+        whitePawn.canMove = true;
+      }
+      // Displays two circles when white pawn can both move forward and capture left
+       else if (whiteTurn && whitePawn.pawnSelected && checkSquare(whitePawn.cols - 1, whitePawn.rows - 1) === "black" && checkSquare(whitePawn.cols, whitePawn.rows - 1) === "empty") {
+        circle(whitePawn.cols * cellSize + cellSize/2, whitePawn.rows * cellSize - cellSize/2, cellSize/4);
+        circle((whitePawn.cols - 1) * cellSize + cellSize/2, whitePawn.rows * cellSize - cellSize/2, cellSize/4);
+        whitePawn.canMove = true;
+      }
+       // Displays two circles when white pawn can both move forward and capture right
+       else if (whiteTurn && whitePawn.pawnSelected && checkSquare(whitePawn.cols + 1, whitePawn.rows - 1) === "black" && checkSquare(whitePawn.cols, whitePawn.rows - 1) === "empty") {
+        circle(whitePawn.cols * cellSize + cellSize/2, whitePawn.rows * cellSize - cellSize/2, cellSize/4);
+        circle((whitePawn.cols + 1) * cellSize + cellSize/2, whitePawn.rows * cellSize - cellSize/2, cellSize/4);
+        whitePawn.canMove = true;
+      }
+      // Displays one circle when white pawn can capture left
+      else if (whiteTurn && whitePawn.pawnSelected && checkSquare(whitePawn.cols - 1, whitePawn.rows - 1) === "black") {
+        circle((whitePawn.cols - 1) * cellSize + cellSize/2, whitePawn.rows * cellSize - cellSize/2, cellSize/4);
+        whitePawn.canMove = true;
+      }
+       // Displays one circle when white pawn can capture right
+      else if (whiteTurn && whitePawn.pawnSelected && checkSquare(whitePawn.cols + 1, whitePawn.rows - 1) === "black") {
+        circle((whitePawn.cols + 1) * cellSize + cellSize/2, whitePawn.rows * cellSize - cellSize/2, cellSize/4);
+        whitePawn.canMove = true;
+      }
       // Displays two circles for white pawn when allowed (first move)
-      if (whiteTurn && whitePawn.pawnSelected && whitePawn.firstMove && emptySquare(whitePawn.cols, whitePawn.rows - 1) === true && emptySquare(whitePawn.cols, whitePawn.rows - 2) === true) {
-        noStroke();
-        fill("grey");
+      else if (whiteTurn && whitePawn.pawnSelected && whitePawn.firstMove && checkSquare(whitePawn.cols, whitePawn.rows - 1) === "empty" && checkSquare(whitePawn.cols, whitePawn.rows - 2) === "empty") {
         circle(whitePawn.cols * cellSize + cellSize/2, whitePawn.rows * cellSize - cellSize/2, cellSize/4);
         circle(whitePawn.cols * cellSize + cellSize/2, (whitePawn.rows - 1) * cellSize - cellSize/2, cellSize/4);
         whitePawn.canMove = true;
       }
       // Displays one circle for white pawn when allowed
-      else if (whiteTurn && whitePawn.pawnSelected && emptySquare(whitePawn.cols, whitePawn.rows - 1) === true) {
-        noStroke();
-        fill("grey");
+      else if (whiteTurn && whitePawn.pawnSelected && checkSquare(whitePawn.cols, whitePawn.rows - 1) === "empty") {
         circle(whitePawn.cols * cellSize + cellSize/2, whitePawn.rows * cellSize - cellSize/2, cellSize/4);
         whitePawn.canMove = true;
-      }
-      // Displays a circle indicating that the white pawn can capture a black one
-      else if (whiteTurn && whitePawn.pawnSelected && emptySquare(whitePawn.cols - 1, whitePawn.rows - 1) === "black") {
-        noStroke();
-        fill("grey");
-        circle((whitePawn.cols - 1) * cellSize + cellSize/2, whitePawn.rows * cellSize - cellSize/2, cellSize/4);
       }
       else {
         whitePawn.canMove = false;
       }
       // Displays two circles for black pawn when allowed (first move)
-      if (blackTurn && blackPawn.pawnSelected && blackPawn.firstMove && emptySquare(blackPawn.cols, blackPawn.rows + 1) === true && emptySquare(blackPawn.cols, blackPawn.rows + 2) === true) {
-        noStroke();
-        fill("grey");
+      if (blackTurn && blackPawn.pawnSelected && blackPawn.firstMove && checkSquare(blackPawn.cols, blackPawn.rows + 1) === "empty" && checkSquare(blackPawn.cols, blackPawn.rows + 2) === "empty") {
         circle(blackPawn.cols * cellSize + cellSize/2, (blackPawn.rows + 2) * cellSize - cellSize/2, cellSize/4);
         circle(blackPawn.cols * cellSize + cellSize/2, (blackPawn.rows + 3) * cellSize - cellSize/2, cellSize/4);
         blackPawn.canMove = true;
       }
       // Displays one circle for white pawn when allowed
-      else if (blackTurn && blackPawn.pawnSelected && emptySquare(blackPawn.cols, blackPawn.rows + 1) === true) {
-        noStroke();
-        fill("grey");
+      else if (blackTurn && blackPawn.pawnSelected && checkSquare(blackPawn.cols, blackPawn.rows + 1) === "empty") {
         circle(blackPawn.cols * cellSize + cellSize/2, (blackPawn.rows + 2) * cellSize - cellSize/2, cellSize/4);
         blackPawn.canMove = true;
       }
